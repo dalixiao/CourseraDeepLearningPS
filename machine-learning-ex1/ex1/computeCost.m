769684086f0524151dbdb208c91654488569797d
dalixiao <daleyshaw96@gmail.com>
2020-03-15 20:17:55 +0800

    linear regression with one variable done

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -11,7 +11,10 @@ J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
+% You should set J to the cost.
+for i = 1:m
+    J = J + 1/(2*m)*( X(i,:)*theta-y(i) )^2;
+end
 
 
 
--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -15,7 +15,13 @@ figure; % open a new figure window
 % Hint: You can use the 'rx' option with plot to have the markers
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
-
+data = load('ex1data1.txt');                % read comma separated data
+x = data(:, 1); y = data(:, 2);
+m = length(y);                              % number of training examples
+plot(x, y, 'rx', 'MarkerSize', 10);         % Plot the data 
+ylabel('Profit in $10,000s');               % Set the y?axis label 
+xlabel('Population of City in 10,000s');    % Set the x?axis label
+         
 
 
 
