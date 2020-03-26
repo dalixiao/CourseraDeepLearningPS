f500e83d3d438b4106367fd6bdd4416666e185ac
dalixiao <daleyshaw96@gmail.com>
2020-03-27 03:22:36 +0800

    finished

--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -15,8 +15,11 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-
-
+prob = zeros(m,1)';
+prob = sigmoid(X*theta);
+prob = prob < 0.5;
+prob = 1 - prob;
+p = prob;
 
 
 
